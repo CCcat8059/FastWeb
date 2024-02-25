d9cc09bed4a0e737e74f4edb482886eace8d5437
CCcat <linnemo920317@gmail.com>
2024-02-25 22:13:35 +0800

    release

--- release.ps1
+++ release.ps1
@@ -9,17 +9,25 @@ $archs = @('x64', 'arm64')
 git tag $version
 git push --tags
 
-Remove-Item ./out/*.zip -Recurse -Force -ErrorAction Ignore
+Remove-Item ./release/*.zip -Recurse -Force -ErrorAction Ignore
 foreach ($arch in $archs) {
 	$releasePath = "./bin/$arch/Release/net8.0-windows"
 
 	dotnet build -c Release /p:Platform=$arch
 
-	Remove-Item "./out/$name/*" -Recurse -Force -ErrorAction Ignore
-	mkdir "./out/$name" -ErrorAction Ignore
-	Copy-Item "$releasePath/$assembly.dll", "$releasePath/plugin.json", "$releasePath/Images", "$releasePath/$assembly.deps.json" "./out/$name" -Recurse -Force
-	Compress-Archive "./out/$name" "./out/$name-$version-$arch.zip" -Force
+	Remove-Item "./release/$name/*" -Recurse -Force -ErrorAction Ignore
+	mkdir "./release/$name" -ErrorAction Ignore
+
+	$files = @(
+		"$releasePath/$assembly.dll",
+		"$releasePath/plugin.json",
+		"$releasePath/Images",
+		"$releasePath/$assembly.deps.json",
+		"$releasePath/webdata.json"
+	)
+	Copy-Item $files "./release/$name" -Recurse -Force
+	Compress-Archive "./release/$name" "./release/$name-$version-$arch.zip" -Force
 }
 
-gh release create $version (Get-ChildItem ./out/*.zip)
+# gh release create $version (Get-ChildItem ./release/*.zip)
 Pop-Location
